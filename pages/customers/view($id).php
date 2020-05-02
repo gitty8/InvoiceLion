5dd0c8fd2deac5a2b99a819553e34de0bf19070c
Joost van der Schee <joost@vdschee.nl>
2020-05-02 13:47:50 +0200

    update

--- pages/customers/view($id).php
+++ pages/customers/view($id).php
@@ -2,4 +2,4 @@
 $data = DB::selectOne('select * from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $id);
 $languages = DB::selectPairs('SELECT `id`,`name` FROM `languages`'); // tenant_id not required
 $invoices = DB::select('select * from `invoices` where `tenant_id` = ? AND `customer_id`=? order by number desc', $_SESSION['user']['tenant_id'], $id);
-$referrals = DB::select('select * from `subscriptions` WHERE `tenant_id` = ? AND `referral_customer_id`=? order by name', $_SESSION['user']['tenant_id'], $id);
+$referrals = DB::select('select * from `subscriptions` WHERE `tenant_id` = ? AND `referral_customer_id`=? AND (canceled > now() or canceled is null) order by name', $_SESSION['user']['tenant_id'], $id);
